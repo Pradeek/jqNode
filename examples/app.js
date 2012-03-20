1cc51ded5ceb3ba51fdb73989718eed1de56e26c
Pradeek <jpradeek@gmail.com>
2012-03-20 10:12:09 +0530

    Added sinatra-like routing

--- examples/app.js
+++ examples/app.js
@@ -4,12 +4,21 @@ $("/").get(function(request, response) {
 	$.writeFile("index.html");
 });
 
+
+$("/test").get(function(request, response, data) { 
+	$.write(data['q']);
+});
+
 $("/test").post(function(request, response, data) {
 	console.log(data);
 });
 
-$("/sample").get(function(request, response) {
-	$.redirect("/");
+$("/test/:id/sample/:val").get(function(request, response, data) {
+	$.write(data['id'] + " | " + data['val']);
+});
+
+$("/sample/:id").get(function(request, response, data) {
+	$.write(data['id']);
 });
 
 $.start({
